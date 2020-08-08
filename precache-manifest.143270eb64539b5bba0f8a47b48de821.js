7a23274f25411386d583654fabb1ae2faf025a7b
Julien Mayer <jalexismayer@gmail.com>
2020-08-08 17:29:19 +0900

    Updates

---
+++ precache-manifest.143270eb64539b5bba0f8a47b48de821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1102ea7cda2fdc8635ad45e1cb464e",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "e31df886acd35adf3661",
+    "url": "/Todo/static/css/main.bf94c422.chunk.css"
+  },
+  {
+    "revision": "724630aa3a4945c865b6",
+    "url": "/Todo/static/js/2.d78a2237.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.d78a2237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31df886acd35adf3661",
+    "url": "/Todo/static/js/main.efa0d4f2.chunk.js"
+  },
+  {
+    "revision": "4f08a680617c00413cc3",
+    "url": "/Todo/static/js/runtime-main.a575cee7.js"
+  }
+]);
